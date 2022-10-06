f2f04a5c004cd4f6755af46c08869aad7747a4f7
Tanquex <114451376+Tanquex@users.noreply.github.com>
2022-10-06 02:40:42 -0500

    P2Act1

---
+++ hello.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="./css/style.css">
+<br><br>
+<div id="hola" >
+    <?php
+        echo 'Hello World 🖐 ';
+        echo '<br>';
+    ?>
+</div>
+<br><br>
+<div id="otros">
+    <?php
+    print("Hola a todos");
+    echo'<br>';
+    printf("Bienvenidos a este mundo llamado PHP");
+    echo'<br>';
+    print("Soy Hackiman");
+    echo'<br>';
+    printf("Alumno: carlos Samael Aguayo Santana");
+    
+    ?>
+</div>
